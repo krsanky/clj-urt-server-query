84a3a1628e0b83ed697c027cc5e48099c5ad947d
paul wisehart <paul@oldcode.org>
2016-12-20 14:26:38 -0500

    urtctf query works using clojure to call old java code. (master query doesnt work)

--- project.clj
+++ project.clj
@@ -3,7 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.apache.commons/commons-lang3 "3.3.2"]
+                 [aleph "0.4.1"]]
   :main ^:skip-aot query-ut.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+  :source-paths ["src/clojure"]
+  :java-source-paths ["src/java"])
--- src/query_ut/core.clj
+++
@@ -1,7 +0,0 @@
-(ns query-ut.core
-  (:gen-class))
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
