dccadb8832441346ca927fa855d87c69fcbe4ee1
paul wisehart <paul@oldcode.org>
2016-12-22 18:18:44 -0500

    take java notion out of project

--- project.clj
+++ project.clj
@@ -3,13 +3,11 @@
   :url "http://example.com/FIXME"
   :license {}
 
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.apache.commons/commons-lang3 "3.3.2"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
 
   :main ^:skip-aot main
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}
 
-  :source-paths ["src/clojure"]
-  :java-source-paths ["src/java"])
-
+  :source-paths ["src"]
+)
