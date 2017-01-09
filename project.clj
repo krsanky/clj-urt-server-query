182f5269118e60ba7b2cbe833710cd31e28ae53e
paul wisehart <paul@oldcode.org>
2017-01-09 15:39:51 -0500

    can play basic midi stuff using fluid sound font

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {}
 
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [overtone "0.10.1"]
+                 [midi.soundfont "0.1.0"]
+                 [org.bitbucket.daveyarwood/fluid-r3 "0.1.1"]]
 
   :main ^:skip-aot main
   :target-path "target/%s"
